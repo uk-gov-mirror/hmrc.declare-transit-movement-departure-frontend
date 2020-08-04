2720ce7a759d8d9ecafcfaafd8f549b5670e6378
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2020-08-04 12:46:36 +0100

    CTCTRADERS-823:merge with master

--- app/controllers/actions/IdentifierAction.scala
+++ app/controllers/actions/IdentifierAction.scala
@@ -41,13 +41,19 @@ class AuthenticatedIdentifierAction @Inject()(
 
   override def invokeBlock[A](request: Request[A], block: IdentifierRequest[A] => Future[Result]): Future[Result] = {
 
-    implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromHeadersAndSession(request.headers, Some(request.session))
+    implicit val hc: HeaderCarrier = HeaderCarrierConverter
+      .fromHeadersAndSession(request.headers, Some(request.session))
 
-    authorised().retrieve(Retrievals.internalId) {
-      _.map {
-        internalId => block(IdentifierRequest(request, EoriNumber(internalId)))
-      }.getOrElse(throw new UnauthorizedException("Unable to retrieve internal Id"))
-    } recover {
+    authorised(Enrolment(config.enrolmentKey))
+      .retrieve(Retrievals.authorisedEnrolments) {
+        enrolments =>
+          val eoriNumber: String = (for {
+            enrolment <- enrolments.enrolments.find(_.key.equals(config.enrolmentKey))
+            identifier <- enrolment.getIdentifier(config.enrolmentIdentifierKey)
+          } yield identifier.value).getOrElse(throw InsufficientEnrolments(s"Unable to retrieve enrolment for ${config.enrolmentIdentifierKey}"))
+
+          block(IdentifierRequest(request, EoriNumber(eoriNumber)))
+      } recover {
       case _: NoActiveSession =>
         Redirect(config.loginUrl, Map("continue" -> Seq(config.loginContinueUrl)))
       case _: AuthorisationException =>
