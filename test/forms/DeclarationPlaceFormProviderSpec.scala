ded24cfb82d6f4edb616842b95331cf0d463e19c
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2020-08-06 12:09:33 +0100

    CTCTRADERS-670:shpw only one error message at a time

--- test/forms/DeclarationPlaceFormProviderSpec.scala
+++ test/forms/DeclarationPlaceFormProviderSpec.scala
@@ -50,18 +50,18 @@ class DeclarationPlaceFormProviderSpec extends StringFieldBehaviours {
 
     "must not bind strings longer than max length" in {
 
-      val expectedError = FormError(fieldName, lengthKey, Seq(maxLength))
+      val expectedError = List(FormError(fieldName, lengthKey, Seq(maxLength)))
 
       forAll(stringsLongerThan(maxLength + 1)) {
         string =>
           val result = form.bind(Map(fieldName -> string)).apply(fieldName)
-          result.errors must contain(expectedError)
+          result.errors mustBe expectedError
       }
     }
 
     "must not bind strings that do not match regex" in {
 
-      val expectedError = FormError(fieldName, invalidKey, Seq(postCodeRegex))
+      val expectedError = List(FormError(fieldName, invalidKey, Seq(postCodeRegex)))
 
       val genInvalidString: Gen[String] = {
         stringsWithMaxLength(maxLength) suchThat (!_.matches(postCodeRegex))
@@ -70,7 +70,7 @@ class DeclarationPlaceFormProviderSpec extends StringFieldBehaviours {
       forAll(genInvalidString) {
         invalidString =>
           val result = form.bind(Map(fieldName -> invalidString)).apply(fieldName)
-          result.errors must contain(expectedError)
+          result.errors mustBe expectedError
       }
     }
   }
