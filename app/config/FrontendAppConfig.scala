335162278e3e9809e8a146180faf30564b72593e
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2020-08-11 09:59:06 +0100

    CTCTRADERS-660: updtae managefrontend config change

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -42,7 +42,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   lazy val enrolmentKey: String     = configuration.get[String]("keys.enrolmentKey")
   lazy val enrolmentIdentifierKey: String = "VATRegNoTURN"
 
-  lazy val manageTransitMovementsUrl: String = configuration.get[String]("urls.manageTransitMovementsFrontend")
+  lazy val manageTransitMovementsUrl: String = configuration.get[Service]("microservice.services.manageFrontend").fullServiceUrl
 
   lazy val languageTranslationEnabled: Boolean =
     configuration.get[Boolean]("microservice.services.features.welsh-translation")
--- app/config/Service.scala
+++ app/config/Service.scala
@@ -20,13 +20,17 @@ import play.api.{ConfigLoader, Configuration}
 
 import scala.language.implicitConversions
 
-final case class Service(host: String, port: String, protocol: String) {
+final case class Service(host: String, port: String, protocol: String, startUrl: String) {
 
   def baseUrl: String =
     s"$protocol://$host:$port"
 
+  def fullServiceUrl: String =
+    s"$baseUrl/$startUrl"
+
   override def toString: String =
-    baseUrl
+    fullServiceUrl
+
 }
 
 object Service {
@@ -39,8 +43,9 @@ object Service {
         val host     = service.get[String]("host")
         val port     = service.get[String]("port")
         val protocol = service.get[String]("protocol")
+        val startUrl = service.get[String]("startUrl")
 
-        Service(host, port, protocol)
+        Service(host, port, protocol, startUrl)
   }
 
   implicit def convertToString(service: Service): String =
