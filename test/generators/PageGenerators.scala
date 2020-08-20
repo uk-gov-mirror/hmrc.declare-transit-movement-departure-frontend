ca4171852e14d1fb8099b6bfa8b8e7604dcab604
Kevin <29205960+kevincoatsworth@users.noreply.github.com>
2020-08-20 13:46:29 +0100

    CTCTRADERS-809: merged master

--- test/generators/PageGenerators.scala
+++ test/generators/PageGenerators.scala
@@ -21,6 +21,9 @@ import pages._
 
 trait PageGenerators {
 
+  implicit lazy val arbitraryIsPrincipalEoriKnownPage: Arbitrary[IsPrincipalEoriKnownPage.type] =
+    Arbitrary(IsPrincipalEoriKnownPage)
+
   implicit lazy val arbitraryWhatIsPrincipalEoriPage: Arbitrary[WhatIsPrincipalEoriPage.type] =
     Arbitrary(WhatIsPrincipalEoriPage)
 
