a7a1adb44c062b9c1c7fcb0ae9d74344df8d76c8
snehaprathipat1 <nihaq1@gmail.com>
2020-08-27 12:22:35 +0100

    Add sections to TraderDetailsCheckYourAnswersController

--- app/controllers/TraderDetailsCheckYourAnswersController.scala
+++ app/controllers/TraderDetailsCheckYourAnswersController.scala
@@ -2,12 +2,14 @@ package controllers
 
 import controllers.actions._
 import javax.inject.Inject
-import models.LocalReferenceNumber
-import play.api.i18n.{I18nSupport, MessagesApi}
+import models.{LocalReferenceNumber, UserAnswers}
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import renderer.Renderer
 import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import utils.CheckYourAnswersHelper
+import viewModels.sections.Section
 
 import scala.concurrent.ExecutionContext
 
@@ -22,9 +24,30 @@ class TraderDetailsCheckYourAnswersController @Inject()(
 
   def onPageLoad(lrn: LocalReferenceNumber): Action[AnyContent] = (identify andThen getData(lrn) andThen requireData).async {
     implicit request =>
-
-      val json = Json.obj("lrn" -> lrn)
+      val sections: Seq[Section] = createSections(request.userAnswers)
+      val json = Json.obj("lrn" -> lrn,
+        "sections" -> Json.toJson(sections)
+      )
 
       renderer.render("traderDetailsCheckYourAnswers.njk", json).map(Ok(_))
+  }
+
+  private def createSections(userAnswers: UserAnswers)(implicit messages: Messages): Seq[Section] = {
+    val checkYourAnswersHelper = new CheckYourAnswersHelper(userAnswers)
+
+    Seq(Section(
+      Seq(checkYourAnswersHelper.isPrincipalEoriKnown,
+        checkYourAnswersHelper.principalName,
+        checkYourAnswersHelper.principalAddress,
+        checkYourAnswersHelper.addConsignor,
+        checkYourAnswersHelper.isConsignorEoriKnown,
+        checkYourAnswersHelper.consignorName,
+        checkYourAnswersHelper.consignorEori,
+        checkYourAnswersHelper.addConsignee,
+        checkYourAnswersHelper.isConsigneeEoriKnown,
+        checkYourAnswersHelper.consigneeName,
+        checkYourAnswersHelper.whatIsConsigneeEori,
+      ).flatten
+    ))
   }
 }
