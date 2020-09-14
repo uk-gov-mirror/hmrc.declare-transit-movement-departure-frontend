e007ec7b174574416b43dc2e595aa3cd049af374
kenneth.rayner <kenneth.rayner@digital.hmrc.gov.uk>
2020-09-14 15:26:45 +0100

    CTCTRADERS-964: refactor

--- test/controllers/transportDetails/TransportDetailsCheckYourAnswersControllerSpec.scala
+++ test/controllers/transportDetails/TransportDetailsCheckYourAnswersControllerSpec.scala
@@ -17,19 +17,44 @@
 package controllers.transportDetails
 
 import base.SpecBase
+import connectors.ReferenceDataConnector
 import matchers.JsonMatchers
-import org.mockito.ArgumentCaptor
+import models.TransportModeList
+import models.reference.TransportMode
+import navigation.annotations.TransportDetails
+import navigation.{FakeNavigator, Navigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{times, verify, when}
+import org.mockito.{ArgumentCaptor, Mockito}
 import org.scalatestplus.mockito.MockitoSugar
+import pages.InlandModePage
+import play.api.Application
+import play.api.inject.bind
 import play.api.libs.json.{JsObject, Json}
+import play.api.mvc.{AnyContentAsFormUrlEncoded, Call, Result}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.twirl.api.Html
+import repositories.SessionRepository
 
 import scala.concurrent.Future
 
 class TransportDetailsCheckYourAnswersControllerSpec extends SpecBase with MockitoSugar with JsonMatchers {
+
+  def onwardRoute = Call("GET", "/foo")
+
+  val transportMode1: TransportMode = TransportMode("1", "test1")
+  val transportMode2: TransportMode = TransportMode("2", "test2")
+  val transportModes:TransportModeList = TransportModeList(Seq(transportMode1, transportMode2))
+
+  private val mockReferenceDataConnector: ReferenceDataConnector = mock[ReferenceDataConnector]
+
+  lazy val transportDetailsRoute: String = routes.TransportDetailsCheckYourAnswersController.onPageLoad(lrn).url
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    Mockito.reset(mockReferenceDataConnector)
+  }
 
   "TransportDetailsCheckYourAnswers Controller" - {
 
@@ -37,8 +62,9 @@ class TransportDetailsCheckYourAnswersControllerSpec extends SpecBase with Mocki
 
       when(mockRenderer.render(any(), any())(any()))
         .thenReturn(Future.successful(Html("")))
-
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      when(mockReferenceDataConnector.getTransportModes()(any(), any())).thenReturn(Future.successful(transportModes))
+      val updatedAnswers  = emptyUserAnswers.set(InlandModePage, "1").success.value
+      val application = applicationBuilder(userAnswers = Some(updatedAnswers)).build()
       val request = FakeRequest(GET, routes.TransportDetailsCheckYourAnswersController.onPageLoad(lrn).url)
       val templateCaptor = ArgumentCaptor.forClass(classOf[String])
       val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
@@ -49,12 +75,44 @@ class TransportDetailsCheckYourAnswersControllerSpec extends SpecBase with Mocki
 
       verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
 
-      val expectedJson = Json.obj("lrn" -> lrn)
+      val expectedJson = Json.obj("lrn" -> lrn) ++ Json.obj("text" -> "Sea transport")
+println(s"\n\n\n88888${jsonCaptor.getValue}")
 
       templateCaptor.getValue mustEqual "transportDetailsCheckYourAnswers.njk"
       jsonCaptor.getValue must containJson(expectedJson)
 
       application.stop()
     }
+
+    "must redirect to the next page when valid data is submitted" ignore {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockRenderer.render(any(), any())(any()))
+        .thenReturn(Future.successful(Html("")))
+      when(mockReferenceDataConnector.getTransportModes()(any(), any())).thenReturn(Future.successful(transportModes))
+
+      val application: Application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind(classOf[Navigator]).qualifiedWith(classOf[TransportDetails]).toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionRepository].toInstance(mockSessionRepository),
+            bind[ReferenceDataConnector].toInstance(mockReferenceDataConnector))
+          .build()
+
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        FakeRequest(POST, transportDetailsRoute)
+          .withFormUrlEncodedBody(("1", "test"))
+
+      val result: Future[Result] = route(application, request).value
+
+      status(result) mustEqual SEE_OTHER
+      println
+
+      redirectLocation(result).value mustEqual onwardRoute.url
+
+      application.stop()
+    }
+
   }
 }
