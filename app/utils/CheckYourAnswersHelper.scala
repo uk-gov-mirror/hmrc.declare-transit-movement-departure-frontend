e0cfe615c5ece9c87b988d5d4bfa14ae0f00b2a1
kenneth.rayner <kenneth.rayner@digital.hmrc.gov.uk>
2020-09-16 07:53:12 +0100

    CTCTRADERS-800 create initial page

--- app/utils/CheckYourAnswersHelper.scala
+++ app/utils/CheckYourAnswersHelper.scala
@@ -22,9 +22,25 @@ import controllers.routes
 import models.{CheckMode, LocalReferenceNumber, UserAnswers}
 import pages._
 import uk.gov.hmrc.viewmodels.SummaryList.{Action, Key, Row, Value}
+import uk.gov.hmrc.viewmodels.Text.Literal
 import uk.gov.hmrc.viewmodels._
 
 class CheckYourAnswersHelper(userAnswers: UserAnswers) {
+
+  def totalPackages: Option[Row] = userAnswers.get(TotalPackagesPage) map {
+    answer =>
+      Row(
+        key     = Key(msg"totalPackages.checkYourAnswersLabel", classes = Seq("govuk-!-width-one-half")),
+        value   = Value(Literal(answer.toString)),
+        actions = List(
+          Action(
+            content            = msg"site.edit",
+            href               = routes.TotalPackagesController.onPageLoad(lrn, CheckMode).url,
+            visuallyHiddenText = Some(msg"site.edit.hidden".withArgs(msg"totalPackages.checkYourAnswersLabel"))
+          )
+        )
+      )
+  }
 
   def addSecurityDetails: Option[Row] = userAnswers.get(AddSecurityDetailsPage) map {
     answer =>
