03ba2b0701507ba7e1f18a74b57b2a1e8b759bfa
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2020-10-21 14:12:35 +0100

    CTCTRADERS-1115: Scaffold PackageType page

---
+++ test/controllers/PackageTypeControllerSpec.scala
@@ -0,0 +1,44 @@
+package controllers
+
+import base.SpecBase
+import matchers.JsonMatchers
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.libs.json.{JsObject, Json}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.twirl.api.Html
+
+import scala.concurrent.Future
+
+class PackageTypeControllerSpec extends SpecBase with MockitoSugar with JsonMatchers {
+
+  "PackageType Controller" - {
+
+    "return OK and the correct view for a GET" in {
+
+      when(mockRenderer.render(any(), any())(any()))
+        .thenReturn(Future.successful(Html("")))
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val request = FakeRequest(GET, routes.PackageTypeController.onPageLoad(lrn).url)
+      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+      val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
+
+      val result = route(application, request).value
+
+      status(result) mustEqual OK
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      val expectedJson = Json.obj("lrn" -> lrn)
+
+      templateCaptor.getValue mustEqual "packageType.njk"
+      jsonCaptor.getValue must containJson(expectedJson)
+
+      application.stop()
+    }
+  }
+}
