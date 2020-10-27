57b8a4f435d14ec6d4bdeefba4fda842222d7b22
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2020-10-27 09:24:26 +0000

    CTCTRADERS-1193: Resolved merge conflicts

--- test/navigation/AddItemsNavigatorSpec.scala
+++ test/navigation/AddItemsNavigatorSpec.scala
@@ -129,7 +129,6 @@ class AddItemsNavigatorSpec extends SpecBase with ScalaCheckPropertyChecks with 
       }
 
       "PackageJourney" - {
-
         "PackageType" - {
 
           "must go to HowManyPackages when PackageType code isn't bulk or unpacked" in {
@@ -162,7 +161,6 @@ class AddItemsNavigatorSpec extends SpecBase with ScalaCheckPropertyChecks with 
             }
           }
         }
-
         "HowManyPackages" - {
 
           "must go to DeclareMark when PackageType code isn't bulk or unpacked" in {
@@ -217,261 +215,126 @@ class AddItemsNavigatorSpec extends SpecBase with ScalaCheckPropertyChecks with 
           }
 
         }
-
         "DeclareNumberOfPackages" - {
-
-          "must go from AddAnotherItem page to " - {
-
-            "to ItemDescription page if the answer is 'Yes'" in {
-              forAll(arbitrary[UserAnswers]) {
-                answers =>
-                  val updatedAnswer = answers.set(AddAnotherItemPage, false).success.value
-                  navigator
-                    .nextPage(AddAnotherItemPage, NormalMode, updatedAnswer)
-                    .mustBe(mainRoutes.DeclarationSummaryController.onPageLoad(answers.id))
-              }
-            }
-
-            "to task list page if the answer is 'No'" in {
-              forAll(arbitrary[UserAnswers]) {
-                answers =>
-                  val updatedAnswer = answers
-                    .set(AddAnotherItemPage, true)
-                    .success
-                    .value
-                    .set(ItemDescriptionPage(index), "test")
-                    .success
-                    .value
-                  "must go to HowManyPackages if answer is 'Yes'" in {
-                    forAll(arbitrary[UserAnswers]) {
-                      answers =>
-                        val updatedAnswers = answers
-                          .set(DeclareNumberOfPackagesPage(index, index), true)
-                          .success
-                          .value
-
-                        navigator
-                          .nextPage(DeclareNumberOfPackagesPage(index, index), NormalMode, updatedAnswers)
-                          .mustBe(routes.HowManyPackagesController.onPageLoad(answers.id, index, index, NormalMode))
-                    }
-                  }
-
-                  "must go to AddMark if answer is 'No' and PackageType is bulk" in {
-                    forAll(arbitrary[UserAnswers], arbitraryBulkPackageType.arbitrary) {
-                      (answers, packageType) =>
-                        val updatedAnswers = answers
-                          .set(DeclareNumberOfPackagesPage(index, index), false)
-                          .success
-                          .value
-                          .set(PackageTypePage(index, index), packageType.code)
-                          .success
-                          .value
-
-                        navigator
-                          .nextPage(DeclareNumberOfPackagesPage(index, index), NormalMode, updatedAnswers)
-                          .mustBe(routes.AddMarkController.onPageLoad(answers.id, index, index, NormalMode))
-                    }
-                  }
-
-                  "must go to TotalPieces if answer is 'No' and PackageType is unpacked" in {
-                    forAll(arbitrary[UserAnswers], arbitraryUnPackedPackageType.arbitrary) {
-                      (answers, packageType) =>
-                        val updatedAnswers = answers
-                          .set(DeclareNumberOfPackagesPage(index, index), false)
-                          .success
-                          .value
-                          .set(PackageTypePage(index, index), packageType.code)
-                          .success
-                          .value
-
-                        navigator
-                          .nextPage(DeclareNumberOfPackagesPage(index, index), NormalMode, updatedAnswers)
-                          .mustBe(routes.TotalPiecesController.onPageLoad(answers.id, index, index, NormalMode))
-                    }
-                  }
-
-              }
-
-              "TotalPieces" - {
-
-                "must go to AddMark" in {
-                  forAll(arbitrary[UserAnswers], arbitrary[Int]) {
-                    (answers, totalPieces) =>
-                      val updatedAnswers = answers
-                        .set(TotalPackagesPage, totalPieces)
-                        .success
-                        .value
-
-                      navigator
-                        .nextPage(TotalPiecesPage(index, index), NormalMode, updatedAnswers)
-                        .mustBe(routes.AddMarkController.onPageLoad(answers.id, index, index, NormalMode))
-                  }
-                }
-              }
-
-              "AddMark" - {
-
-                "must go to DeclareMark if answers is 'Yes'" in {
-                  forAll(arbitrary[UserAnswers]) {
-                    answers =>
-                      val updatedAnswers = answers
-                        .set(AddMarkPage(index, index), true)
-                        .success
-                        .value
-
-                      navigator
-                        .nextPage(AddMarkPage(index, index), NormalMode, updatedAnswers)
-                        .mustBe(routes.DeclareMarkController.onPageLoad(answers.id, index, index, NormalMode))
-                  }
-                }
-
-                "must go to AddAnotherPackage if answers if 'No'" in {
-                  forAll(arbitrary[UserAnswers]) {
-                    answers =>
-                      val updatedAnswers = answers
-                        .set(AddMarkPage(index, index), false)
-                        .success
-                        .value
-
-                      navigator
-                        .nextPage(AddMarkPage(index, index), NormalMode, updatedAnswers)
-                        .mustBe(routes.AddAnotherPackageController.onPageLoad(answers.id, index, index, NormalMode))
-                  }
-                }
-              }
-
-              "DeclareMark" - {
-
-                "must go to AddAnotherPackage" in {
-                  forAll(arbitrary[UserAnswers], arbitrary[String]) {
-                    (answers, declareMark) =>
-                      val updatedAnswers = answers
-                        .set(DeclareMarkPage(index, index), declareMark)
-                        .success
-                        .value
-
-                      navigator
-                        .nextPage(DeclareMarkPage(index, index), NormalMode, updatedAnswers)
-                        .mustBe(routes.AddAnotherPackageController.onPageLoad(answers.id, index, index, NormalMode))
-                  }
-                }
-              }
-
-              "AddAnotherPackage" - {
-
-                "must go to PackageType if the answer is 'Yes' and increment package index" in {
-                  forAll(arbitrary[UserAnswers]) {
-                    (answers) =>
-                      val updatedAnswers = answers
-                        .set(AddAnotherPackagePage(index, index), true)
-                        .success
-                        .value
-
-                      val nextPackageIndex = Index(index.position + 1)
-
-                      navigator
-                        .nextPage(AddAnotherPackagePage(index, index), NormalMode, updatedAnswers)
-                        .mustBe(routes.PackageTypeController.onPageLoad(answers.id, index, nextPackageIndex, NormalMode))
-                  }
-                }
-
-                "must go to ContainerNumber if containers use in overview journey is 'Yes' and answer is 'No'" ignore {
-                  ???
-                }
-
-                "must go to AddSpecialMentions if container use in overview journey is 'No' and answer is 'No'" ignore {
-                  ???
-                }
-              }
-            }
-
-            "must go from AddAnotherItem page to" - {
-
-              "ItemDescription page if the answer is 'Yes'" in {
-                forAll(arbitrary[UserAnswers]) {
-                  answers =>
-                    val updatedAnswer = answers.set(AddAnotherItemPage, false).success.value
-                    navigator
-                      .nextPage(AddAnotherItemPage, NormalMode, updatedAnswer)
-                      .mustBe(mainRoutes.DeclarationSummaryController.onPageLoad(answers.id))
-                }
-              }
-
-              "task list page if the answer is 'No'" in {
-                forAll(arbitrary[UserAnswers]) {
-                  answers =>
-                    val updatedAnswer = answers
-                      .set(AddAnotherItemPage, true)
-                      .success
-                      .value
-                      .set(ItemDescriptionPage(index), "test")
-                      .success
-                      .value
-
-                    navigator
-                      .nextPage(AddAnotherItemPage, NormalMode, updatedAnswer)
-                      .mustBe(routes.ItemDescriptionController.onPageLoad(answers.id, Index(1), NormalMode))
-                }
-              }
-            }
-
-            "must go from ConfirmRemoveItem page to " - {
-
-              "AddAnotherItem page when 'No' is selected and there are more than one item" in {
-                forAll(arbitrary[UserAnswers]) {
-                  answers =>
-                    val updatedAnswers = answers
-                      .set(ItemDescriptionPage(index), "item1")
-                      .success
-                      .value
-                      .set(ItemDescriptionPage(Index(1)), "item2")
-                      .success
-                      .value
-                      .set(AddAnotherItemPage, true)
-                      .success
-                      .value
-                      .set(ConfirmRemoveItemPage, false)
-                      .success
-                      .value
-                    navigator
-                      .nextPage(ConfirmRemoveItemPage, NormalMode, updatedAnswers)
-                      .mustBe(routes.AddAnotherItemController.onPageLoad(updatedAnswers.id))
-                }
-              }
-
-              "AddAnotherItem page when 'Yes' is selected and there are more than one item" in {
-                forAll(arbitrary[UserAnswers]) {
-                  answers =>
-                    val updatedAnswers = answers
-                      .set(ItemDescriptionPage(index), "item1")
-                      .success
-                      .value
-                      .set(ItemDescriptionPage(Index(1)), "item2")
-                      .success
-                      .value
-                      .set(ConfirmRemoveItemPage, true)
-                      .success
-                      .value
-                    navigator
-                      .nextPage(ConfirmRemoveItemPage, NormalMode, updatedAnswers)
-                      .mustBe(routes.AddAnotherItemController.onPageLoad(updatedAnswers.id))
-                }
-              }
-
-              "ItemDescription page when 'Yes' is selected and when all the items are removed" in {
-
-                val updatedAnswers = emptyUserAnswers
-                  .remove(ItemsQuery(index))
+          "must go to HowManyPackages if answer is 'Yes'" in {
+            forAll(arbitrary[UserAnswers]) {
+              answers =>
+                val updatedAnswers = answers
+                  .set(DeclareNumberOfPackagesPage(index, index), true)
                   .success
                   .value
-                  .set(ConfirmRemoveItemPage, true)
-                  .success
-                  .value
+
                 navigator
-                  .nextPage(ConfirmRemoveItemPage, NormalMode, updatedAnswers)
-                  .mustBe(routes.ItemDescriptionController.onPageLoad(updatedAnswers.id, index, NormalMode))
-              }
+                  .nextPage(DeclareNumberOfPackagesPage(index, index), NormalMode, updatedAnswers)
+                  .mustBe(routes.HowManyPackagesController.onPageLoad(answers.id, index, index, NormalMode))
+            }
+          }
+          "must go to AddMark if answer is 'No' and PackageType is bulk" in {
+            forAll(arbitrary[UserAnswers], arbitraryBulkPackageType.arbitrary) {
+              (answers, packageType) =>
+                val updatedAnswers = answers
+                  .set(DeclareNumberOfPackagesPage(index, index), false)
+                  .success
+                  .value
+                  .set(PackageTypePage(index, index), packageType.code)
+                  .success
+                  .value
 
+                navigator
+                  .nextPage(DeclareNumberOfPackagesPage(index, index), NormalMode, updatedAnswers)
+                  .mustBe(routes.AddMarkController.onPageLoad(answers.id, index, index, NormalMode))
+            }
+          }
+          "must go to TotalPieces if answer is 'No' and PackageType is unpacked" in {
+            forAll(arbitrary[UserAnswers], arbitraryUnPackedPackageType.arbitrary) {
+              (answers, packageType) =>
+                val updatedAnswers = answers
+                  .set(DeclareNumberOfPackagesPage(index, index), false)
+                  .success
+                  .value
+                  .set(PackageTypePage(index, index), packageType.code)
+                  .success
+                  .value
+
+                navigator
+                  .nextPage(DeclareNumberOfPackagesPage(index, index), NormalMode, updatedAnswers)
+                  .mustBe(routes.TotalPiecesController.onPageLoad(answers.id, index, index, NormalMode))
+            }
+          }
+
+        }
+        "TotalPieces" - {
+          "must go to AddMark" in {
+            forAll(arbitrary[UserAnswers], arbitrary[Int]) {
+              (answers, totalPieces) =>
+                val updatedAnswers = answers
+                  .set(TotalPackagesPage, totalPieces)
+                  .success
+                  .value
+
+                navigator
+                  .nextPage(TotalPiecesPage(index, index), NormalMode, updatedAnswers)
+                  .mustBe(routes.AddMarkController.onPageLoad(answers.id, index, index, NormalMode))
+            }
+          }
+        }
+        "AddMark" - {
+          "must go to DeclareMark if answers is 'Yes'" in {
+            forAll(arbitrary[UserAnswers]) {
+              answers =>
+                val updatedAnswers = answers
+                  .set(AddMarkPage(index, index), true)
+                  .success
+                  .value
+
+                navigator
+                  .nextPage(AddMarkPage(index, index), NormalMode, updatedAnswers)
+                  .mustBe(routes.DeclareMarkController.onPageLoad(answers.id, index, index, NormalMode))
+            }
+          }
+          "must go to AddAnotherPackage if answers if 'No'" in {
+            forAll(arbitrary[UserAnswers]) {
+              answers =>
+                val updatedAnswers = answers
+                  .set(AddMarkPage(index, index), false)
+                  .success
+                  .value
+
+                navigator
+                  .nextPage(AddMarkPage(index, index), NormalMode, updatedAnswers)
+                  .mustBe(routes.AddAnotherPackageController.onPageLoad(answers.id, index, index, NormalMode))
+            }
+          }
+        }
+        "DeclareMark" - {
+          "must go to AddAnotherPackage" in {
+            forAll(arbitrary[UserAnswers], arbitrary[String]) {
+              (answers, declareMark) =>
+                val updatedAnswers = answers
+                  .set(DeclareMarkPage(index, index), declareMark)
+                  .success
+                  .value
+
+                navigator
+                  .nextPage(DeclareMarkPage(index, index), NormalMode, updatedAnswers)
+                  .mustBe(routes.AddAnotherPackageController.onPageLoad(answers.id, index, index, NormalMode))
+            }
+          }
+        }
+        "AddAnotherPackage" - {
+          "must go to PackageType if the answer is 'Yes' and increment package index" in {
+            forAll(arbitrary[UserAnswers]) {
+              answers =>
+                val updatedAnswers = answers
+                  .set(AddAnotherPackagePage(index, index), true)
+                  .success
+                  .value
+
+                val nextPackageIndex = Index(index.position + 1)
+
+                navigator
+                  .nextPage(AddAnotherPackagePage(index, index), NormalMode, updatedAnswers)
+                  .mustBe(routes.PackageTypeController.onPageLoad(answers.id, index, nextPackageIndex, NormalMode))
             }
           }
         }
@@ -616,6 +479,7 @@ class AddItemsNavigatorSpec extends SpecBase with ScalaCheckPropertyChecks with 
               .mustBe(routes.ItemsCheckYourAnswersController.onPageLoad(answers.id, index))
         }
       }
+
       "PackageJourney" - {
         "PackageType" - {
 
