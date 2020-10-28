18050d97f41706ec8abf2c48af4b19e722a59ebc
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2020-10-28 09:25:23 +0000

    CTCTRADERS-1229:update controller test

--- test/controllers/addItems/previousReferences/AddAdministrativeReferenceControllerSpec.scala
+++ test/controllers/addItems/previousReferences/AddAdministrativeReferenceControllerSpec.scala
@@ -16,7 +16,7 @@
 
 package controllers.addItems.previousReferences
 
-import base.SpecBase
+import base.{MockNunjucksRendererApp, SpecBase}
 import controllers.{routes => mainRoutes}
 import forms.AddAdministrativeReferenceFormProvider
 import matchers.JsonMatchers
@@ -39,7 +39,7 @@ import uk.gov.hmrc.viewmodels.{NunjucksSupport, Radios}
 
 import scala.concurrent.Future
 
-class AddAdministrativeReferenceControllerSpec extends SpecBase with MockitoSugar with NunjucksSupport with JsonMatchers {
+class AddAdministrativeReferenceControllerSpec extends SpecBase with MockNunjucksRendererApp with MockitoSugar with NunjucksSupport with JsonMatchers {
 
   def onwardRoute = Call("GET", "/foo")
 
--- test/generators/UserAnswersGenerator.scala
+++ test/generators/UserAnswersGenerator.scala
@@ -31,7 +31,7 @@ trait UserAnswersGenerator extends TryValues {
   self: Generators =>
 
   lazy val generators: Seq[Gen[(QuestionPage[_], JsValue)]] =
-      arbitrary[(AddAdministrativeReferencePage, JsValue)] ::
+    arbitrary[(AddAdministrativeReferencePage, JsValue)] ::
       arbitrary[(ConfirmRemoveItemPage.type, JsValue)] ::
       arbitrary[(AddItemsSameConsignorForAllItemsPage.type, JsValue)] ::
       arbitrary[(AddItemsSameConsigneeForAllItemsPage.type, JsValue)] ::
