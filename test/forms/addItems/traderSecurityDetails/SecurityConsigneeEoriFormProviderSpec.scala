3cd94bba1f14246d281b78bcee9b5571f097947a
kenneth.rayner <kenneth.rayner@digital.hmrc.gov.uk>
2021-01-20 12:58:41 +0000

    CTCTRADERS-regexFix: updagte regex generator in SecurityConsigneeEoriFormProviderSpec

--- test/forms/addItems/traderSecurityDetails/SecurityConsigneeEoriFormProviderSpec.scala
+++ test/forms/addItems/traderSecurityDetails/SecurityConsigneeEoriFormProviderSpec.scala
@@ -21,6 +21,7 @@ import forms.Constants._
 import forms.behaviours.StringFieldBehaviours
 import org.scalacheck.Gen
 import play.api.data.FormError
+import wolfendale.scalacheck.regexp.RegexpGen
 
 class SecurityConsigneeEoriFormProviderSpec extends SpecBase with StringFieldBehaviours {
 
@@ -43,7 +44,7 @@ class SecurityConsigneeEoriFormProviderSpec extends SpecBase with StringFieldBeh
     behave like fieldWithMaxLength(
       form,
       fieldName,
-      maxLength   = maxLengthEoriNumber,
+      maxLength = maxLengthEoriNumber,
       lengthError = FormError(fieldName, lengthKey, Seq(maxLengthEoriNumber))
     )
 
@@ -74,16 +75,14 @@ class SecurityConsigneeEoriFormProviderSpec extends SpecBase with StringFieldBeh
       val expectedError =
         List(FormError(fieldName, invalidFormatKey, Seq(eoriNumberRegex)))
 
-      val genInvalidString: Gen[String] = {
-        alphaNumericWithMaxLength(maxLengthEoriNumber).map(_.toUpperCase) suchThat (!_.matches(eoriNumberRegex))
-      }
+      val invalidString = "^[0-9]{2}[0-9a-zA-Z]{1,15}"
+      val invalidStringGenerator = RegexpGen.from(invalidString)
 
-      forAll(genInvalidString) {
+      forAll(invalidStringGenerator) {
         invalidString =>
           val result = form.bind(Map(fieldName -> invalidString)).apply(fieldName)
           result.errors mustBe expectedError
       }
     }
-
   }
 }
