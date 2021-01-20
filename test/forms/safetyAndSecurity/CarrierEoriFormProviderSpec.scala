a4d1284d98b3d4f20b9f8c79e8961302cac3f631
Kevin <29205960+kevincoatsworth@users.noreply.github.com>
2021-01-20 15:56:11 +0000

    CTCTRADERS-RegexFix: Amended regex in CarrierEoriFormProviderSpec.

--- test/forms/safetyAndSecurity/CarrierEoriFormProviderSpec.scala
+++ test/forms/safetyAndSecurity/CarrierEoriFormProviderSpec.scala
@@ -20,6 +20,7 @@ import forms.Constants.{eoriNumberRegex, maxLengthEoriNumber, validEoriCharacter
 import forms.behaviours.StringFieldBehaviours
 import org.scalacheck.Gen
 import play.api.data.FormError
+import wolfendale.scalacheck.regexp.RegexpGen
 
 class CarrierEoriFormProviderSpec extends StringFieldBehaviours {
 
@@ -73,11 +74,10 @@ class CarrierEoriFormProviderSpec extends StringFieldBehaviours {
       val expectedError =
         List(FormError(fieldName, invalidFormatKey, Seq(eoriNumberRegex)))
 
-      val genInvalidString: Gen[String] = {
-        alphaNumericWithMaxLength(maxLengthEoriNumber).map(_.toUpperCase) suchThat (!_.matches(eoriNumberRegex))
-      }
+      val invalidString          = "^[0-9]{2}[0-9a-zA-Z]{1,15}"
+      val invalidStringGenerator = RegexpGen.from(invalidString)
 
-      forAll(genInvalidString) {
+      forAll(invalidStringGenerator) {
         invalidString =>
           val result = form.bind(Map(fieldName -> invalidString)).apply(fieldName)
           result.errors mustBe expectedError
