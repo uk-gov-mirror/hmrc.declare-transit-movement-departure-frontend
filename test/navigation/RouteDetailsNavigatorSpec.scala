a73fd3f9ac2e3eb198bf6336e910d6e1ee3cf85d
Kevin <29205960+kevincoatsworth@users.noreply.github.com>
2021-03-10 15:47:47 +0000

    CTCTRADERS-1840: Added and amended navigation.

--- test/navigation/RouteDetailsNavigatorSpec.scala
+++ test/navigation/RouteDetailsNavigatorSpec.scala
@@ -75,12 +75,22 @@ class RouteDetailsNavigatorSpec extends SpecBase with ScalaCheckPropertyChecks w
           }
         }
 
-        "must go from Destination Office Page to Add another transit office page" in {
+        "must go from Destination Office Page to Office Of Transit Country page" in {
 
           forAll(arbitrary[UserAnswers]) {
             answers =>
               navigator
                 .nextPage(DestinationOfficePage, NormalMode, answers)
+                .mustBe(routes.OfficeOfTransitCountryController.onPageLoad(answers.id, NormalMode))
+          }
+        }
+
+        "must go from Office Of Transit Country to Add Another Transit Office page" in {
+
+          forAll(arbitrary[UserAnswers]) {
+            answers =>
+              navigator
+                .nextPage(OfficeOfTransitCountryPage, NormalMode, answers)
                 .mustBe(routes.AddAnotherTransitOfficeController.onPageLoad(answers.id, index, NormalMode))
           }
         }
@@ -274,6 +284,18 @@ class RouteDetailsNavigatorSpec extends SpecBase with ScalaCheckPropertyChecks w
 
       }
 
+      "Must go from Movement Destination Country to Destination Office" in {
+
+        forAll(arbitrary[UserAnswers]) {
+          answers =>
+            navigator
+              .nextPage(MovementDestinationCountryPage, CheckMode, answers)
+              .mustBe(routes.DestinationOfficeController.onPageLoad(answers.id, CheckMode))
+
+        }
+
+      }
+
       "Must go from Destination Office to Router Details Check Your Answers" in {
 
         forAll(arbitrary[UserAnswers]) {
@@ -281,6 +303,18 @@ class RouteDetailsNavigatorSpec extends SpecBase with ScalaCheckPropertyChecks w
             navigator
               .nextPage(DestinationOfficePage, CheckMode, answers)
               .mustBe(routes.RouteDetailsCheckYourAnswersController.onPageLoad(answers.id))
+
+        }
+
+      }
+
+      "Must go from Office Of Transit Country to Add Another Transit Office" in {
+
+        forAll(arbitrary[UserAnswers]) {
+          answers =>
+            navigator
+              .nextPage(OfficeOfTransitCountryPage, CheckMode, answers)
+              .mustBe(routes.AddAnotherTransitOfficeController.onPageLoad(answers.id, index, CheckMode))
 
         }
 
